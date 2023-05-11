1c0a3c60e7f2bd5d7887ceeb80d5df68fe6ce66f
BrightOlawale <oolawalebright@gmail.com>
2023-05-11 05:25:36 +0100

    feat: Web server setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,22 +1,20 @@
 #!/usr/bin/env bash
-# Script that sets up your web servers for the deployment of web_static
+# Script that configures Nginx server with some folders and files
 
-html = "
+html="
 <html>
-  <head>
-  </head>
-  <body>
-    <h1>Hello Olatunji!</h1>
-  </body>
+    <body>
+        <p>Hello Olatunji!</p>
+    </body>
+</html>
 "
-
-sudo apt-get update
+sudo apt-get -y update
 sudo apt-get -y install nginx
 sudo service nginx start
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-echo "$html" | sudo tee /data/web_static/releases/test/index.html
+printf "%s" "$html" >> /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
